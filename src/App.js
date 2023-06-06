55bb88e41fd9f3703ae1099a5595667803cbc1c6
unknown <gunshot1st@gmail.com>
2023-06-06 12:44:05 +0700

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Player from './components/player';
+function App() {
+  return (
+    <div className='App'>
+      <Navigation/>
+       <Player/>
+    </div>
+  );
+}
+
+export default App;
